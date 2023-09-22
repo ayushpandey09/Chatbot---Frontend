a453df6fd11e653cdddec0c4fabaa1e246ced9b3
ayushpandey09 <ayushpandey.3july@gmail.com>
2023-09-22 17:49:04 +0530

    UI changes

--- src/component/chatbox/Chatbox.js
+++ src/component/chatbox/Chatbox.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
 import './Chatbox.css';
 import axios from 'axios';
+import MicIcon from '@mui/icons-material/Mic';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import Instruction from './Instruction';
 
 function ChatBox() {
-  
+
   //state to store all the message - prompt and response
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
-  //isContinued -->false for 1st promt ; True ---> for contiuation to previous question
+  //isContinued -->false for 1st promt ; True ---> for continuation to previous question
   const [isContinued, setIsContinued] = useState(false);
-  const [lastQuestion, setLastQuestion]  = useState('');
+  const [lastQuestion, setLastQuestion] = useState('');
 
   //for voice chat
- // const [transcript, setTranscript] = useState('');
+  // const [transcript, setTranscript] = useState('');
   const [isRecording, setIsRecording] = useState(false);
   const [recognition, setRecognition] = useState(null);
 
@@ -28,7 +31,7 @@ function ChatBox() {
     // Event handlers for voice recognition
     recognitionInstance.onresult = (event) => {
       const transcript = event.results[0][0].transcript;
-     // setTranscript(transcript);
+      // setTranscript(transcript);
       setNewMessage(transcript);
     };
 
@@ -50,7 +53,13 @@ function ChatBox() {
     }
     setIsRecording(false);
   };
- 
+
+  const handleKey = (event) => {
+    if (event.key === 'Enter' || event.keyCode === 13) {
+      handleSendMessage();
+    }
+  }
+
 
   // Function to send a user message
   //Summary - multiple API calling 
@@ -61,147 +70,168 @@ function ChatBox() {
   const handleSendMessage = () => {
     if (newMessage.trim() === '') return;
     //to remove '.' from message
-    if(newMessage.includes('.')){
+    if (newMessage.includes('.')) {
       setNewMessage(newMessage.slice(0, -1));
     }
     //set userMessage and add newMessage to message array
-    const userMessage = { text: newMessage, isBot: false };
+    const userMessage = {
+      text: lastQuestion === "Enter your id number" ? "Employee ID: "
+        + newMessage : lastQuestion === "Enter your location" ? "Location: " + newMessage : newMessage, isBot: false
+    };
     setMessages((prevMessages) => [...prevMessages, userMessage]);
     setNewMessage('');
 
+    if (userMessage.text.includes('?')) {
+      userMessage.text = userMessage.text.replace(/\?/g, '');
+    }
+
     //messageBody format should match the backend api format
     const messageBody = {
-        senderName: "user",
-        content: userMessage.text.toLowerCase()
+      senderName: "user",
+      content: userMessage.text.toLowerCase()
     };
-        
-        //check if there is a continuation in the chat or not
-        if(isContinued){
-            setIsContinued(false);
-            let param = newMessage.trim();
-            //remove "." from param
-            if(param.includes('.')){
-              param = param.slice(0, -1);
+
+    //check if there is a continuation in the chat or not
+    if (isContinued) {
+      setIsContinued(false);
+      setLastQuestion("");
+      let param = newMessage.trim();
+      //remove "." from param
+      if (param.includes('.')) {
+        param = param.slice(0, -1);
+      }
+      if (param.includes('?')) {
+        param = param.replace(/\?/g, '');
+      }
+      //continuation for manager prompt
+      if (lastQuestion === "Enter your id number") {
+
+        axios.get(`http://localhost:8080/chatbot/questionmanager/${param}`).then(
+          (response) => {
+            console.log(response.data);
+            const { content, senderName } = response.data;
+            // botResponse = content;
+            const botMessage = { text: content, isBot: true };
+            setMessages((prevMessages) => [...prevMessages, botMessage]);
+          }
+        )
+          .catch(
+            (error) => {
+              console.log(error)
             }
-            //continuation for manager prompt
-            if(lastQuestion ==="Enter your id number"){
-                axios.get(`http://localhost:8080/chatbot/questionmanager/${param}`).then(
-                (response)=>{
-                    console.log(response.data);
-                    const {content,senderName} = response.data;
-                    // botResponse = content;
-                    const botMessage = { text: content, isBot: true };
-                    setMessages((prevMessages) => [...prevMessages, botMessage]);
-                }
-               )
-               .catch(
-                (error)=>{
-                    console.log(error)
-                }
-               )
-               //on the basis of last question next api will call
-            }else if(lastQuestion === "Enter your location"){
-                axios.get(`http://localhost:8080/chatbot/questionithelp/${param}`).then(
-                    (response)=>{
-                        console.log(response.data);
-                        const {content,senderName} = response.data;
-                       // botResponse = content;
-                        const botMessage = { text: content, isBot: true };
-                        setMessages((prevMessages) => [...prevMessages, botMessage]);
-                        
-                    }
-                   )
-                   .catch(
-                    (error)=>{
-                        console.log(error)
-                    }
-                   )
-            }else if(lastQuestion === "Confirm if you want to exit"){
-              if(userMessage.text.toLowerCase().includes("yes")) { 
-              const email = localStorage.getItem('user_email');
-              console.log("user email obtained: "+email);
-              axios.get(`http://localhost:8080/chatbot/chattranscript/${email}`).then(
-                    (response)=>{
-                        const botMessage = { text: response.data, isBot: true };
-                        setMessages((prevMessages) => [...prevMessages, botMessage]);               
-                    }
-                   )
-                   .catch(
-                    (error)=>{
-                        console.log(error)
-                    }
-                   )
+          )
+        //on the basis of last question next api will call
+      } else if (lastQuestion === "Enter your location") {
+
+        axios.get(`http://localhost:8080/chatbot/questionithelp/${param}`).then(
+          (response) => {
+            console.log(response.data);
+            const { content, senderName } = response.data;
+            // botResponse = content;
+            const botMessage = { text: content, isBot: true };
+            setMessages((prevMessages) => [...prevMessages, botMessage]);
+
+          }
+        )
+          .catch(
+            (error) => {
+              console.log(error)
+            }
+          )
+      } else if (lastQuestion === "Confirm if you want to exit") {
+        if (userMessage.text.toLowerCase().includes("yes")) {
+          const email = localStorage.getItem('user_email');
+          console.log("user email obtained: " + email);
+          axios.get(`http://localhost:8080/chatbot/chattranscript/${email}`).then(
+            (response) => {
+              const botMessage = { text: response.data, isBot: true };
+              setMessages((prevMessages) => [...prevMessages, botMessage]);
+            }
+          )
+            .catch(
+              (error) => {
+                console.log(error)
               }
-            }
+            )
         }
-        //post request for initiating chat
-        if(!isContinued){
-        axios.post('http://localhost:8080/chatbot/question',messageBody, {
-            headers: {
-                'Content-Type': 'application/json',
-              },
-        }
-        ).then(
-            (response)=>{
-                console.log(response.data);
-                //Get request for getting the response back
-                    axios.get('http://localhost:8080/chatbot/questionresponse').then(
-                    (response)=>{
-                        console.log(response.data);
-                        //destructoring response.data or fetching (message) content from response
-                        const {content} = response.data;
-                       
-                    //binding the response of the bot 
-                    const botMessage = { text: content, isBot: true };
-                    //updating bot response to final message array(sequence of message)
-                    setMessages((prevMessages) => [...prevMessages, botMessage]);
-                    
-                    //if bot doesn't reconise the prompt it will return null to complete the sequence
-                    if(content === "Sorry... not able to get your prompt")
-                    {
-                        return;
-                    }
-                    //if bot recognise the prompt then it will ask for another input and to get another input 
-                    //isContinue must be true
-                    setIsContinued(true);
-                    setLastQuestion(content);
-                    return ;
-                    //for continuity code will continue from handleSentMessage
-                    }
-                )
-            }
-        ).catch((error) => {
-            console.error('Error sending message:', error);
-            if (error.response) {
-                console.error('Response Data:', error.response.data);
+      }
+    }
+    //post request for initiating chat
+    if (!isContinued) {
+      axios.post('http://localhost:8080/chatbot/question', messageBody, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+      ).then(
+        (response) => {
+          console.log(response.data);
+          //Get request for getting the response back
+          axios.get('http://localhost:8080/chatbot/questionresponse').then(
+            (response) => {
+              console.log(response.data);
+              //destructoring response.data or fetching (message) content from response
+              const { content } = response.data;
+
+              //binding the response of the bot 
+              const botMessage = { text: content, isBot: true };
+              //updating bot response to final message array(sequence of message)
+              setMessages((prevMessages) => [...prevMessages, botMessage]);
+
+              //if bot doesn't reconise the prompt it will return null to complete the sequence
+              if (content === "Sorry... not able to get your prompt") {
+                return;
               }
-          });
+              //if bot recognise the prompt then it will ask for another input and to get another input 
+              //isContinue must be true
+              setIsContinued(true);
+              setLastQuestion(content);
+              return;
+              //for continuity code will continue from handleSentMessage
+            }
+          )
         }
+      ).catch((error) => {
+        console.error('Error sending message:', error);
+        if (error.response) {
+          console.error('Response Data:', error.response.data);
+        }
+      });
+    }
   };
 
   return (
     <div className="chat-box">
       <div className="chat-messages">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`message ${message.isBot ? 'bot' : 'user'}`}
-          >
-            {message.text}
-          </div>
-        ))}
+
+
+        {
+          messages.length === 0 ? (<Instruction/>) :
+            (
+              messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`message ${message.isBot ? 'bot' : 'user'}`}
+                >
+                  {message.text}
+                </div>
+              ))
+            )
+        }
+
+
       </div>
       <div className="input-container">
         <input
           type="text"
           placeholder="Type your message..."
           value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value.toLowerCase)}
+          onChange={(e) => setNewMessage(e.target.value)}
         />
-         <button className ="mx-3" onClick={isRecording ? handleStopRecording : handleStartRecording}>
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
-      </button>
-        <button onClick={handleSendMessage}>Send</button>
+        <button className="mx-3" onClick={isRecording ? handleStopRecording : handleStartRecording}>
+          {isRecording ? <MicOffIcon /> : <MicIcon />}
+        </button>
+        <button onClick={handleSendMessage} onKeyPress={handleKey}>Send</button>
       </div>
     </div>
   );
---
+++ src/component/chatbox/Instruction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Chatbox.css';
+
+function Instruction() {
+  return (
+    <div className='instruction-box'>
+    Instruction :
+     <p className='instruction-text'>* You can ask question like "Who is my manager?" or "Who is IT help?"</p>
+     <p className='instruction-text'>* To complete the conversation you can type "Exit."</p>
+  </div>
+  )
+}
+
+export default Instruction
